228ffc5cd7bbdd04ebc034ee03fcab626ff1188b
Rakesh Yadunath Raikar <rakeshraikar.sagar2002@gmail.com>
2026-02-16 14:00:54 +0530

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,44 @@
+
 'use client'
+import { useEffect } from 'react'
 import { supabase } from '@/lib/supabaseClient'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+  const router = useRouter()
+
+  useEffect(() => {
+    const checkSession = async () => {
+      const { data: { session } } = await supabase.auth.getSession()
+      if (session) {
+        router.push('/dashboard')
+      }
+    }
+
+    checkSession()
+
+    const { data: listener } = supabase.auth.onAuthStateChange(
+      (event, session) => {
+        if (session) {
+          router.push('/dashboard')
+        }
+      }
+    )
+
+    return () => {
+      listener.subscription.unsubscribe()
+    }
+  }, [router])
+
   const login = async () => {
     await supabase.auth.signInWithOAuth({
       provider: 'google',
-       options: {
-      redirectTo: 'http://localhost:3000/dashboard'
-       }
     })
   }
 
   return (
     <div className="h-screen flex items-center justify-center">
-      <button 
+      <button
         onClick={login}
         className="bg-black text-white px-6 py-3 rounded"
       >
@@ -22,4 +47,3 @@ export default function Home() {
     </div>
   )
 }
-
